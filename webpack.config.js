9d74fce8e9c340b599ba0ba4c5834fbd0a15541d
Arjun <arjunsingh8112@gmail.com>
2023-01-05 21:41:35 +0530

    Weather App Project started

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Title',
+            favicon: './src/assets/images/favicon.ico'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|jpeg|svg|gif|ico)/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|odt|ttf|eot|otf)/i,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
