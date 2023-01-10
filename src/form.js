4c9c61328976553ff17672b18e23276c055e6e3c
Arjun <arjunsingh8112@gmail.com>
2023-01-10 11:43:55 +0530

    🪓 Code Splitting and adding loading API data and displaying on Page

--- src/form.js
+++ src/form.js
@@ -1,5 +1,26 @@
+import { loadJson } from "./loadJson.js";
+import { sensitiveData } from "./config.js";
+
 export function createForm() {
-    const form = document.createElement('fomr');
-    const input = document.createElement('input');
-    const label = document.createElement('label');
+    const form = document.querySelector('form');
+    const input = form.querySelector('input');
+    // const submitBtn = form.querySelector('button');
+
+    let locationInput = 'Delhi'
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        locationInput = input.value;
+        input.value = '';
+        
+        console.log(locationInput)
+        loadJson(`https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&APPID=${sensitiveData().appid}&units=metric`)
+    })
+
+    // return {
+    //     locationInput
+    // }
+
+
 }
