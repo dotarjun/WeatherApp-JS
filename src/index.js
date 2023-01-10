c5d351ecfc3d6e0269fa4d95f873e5a1440e5df9
Arjun <arjunsingh8112@gmail.com>
2023-01-10 11:42:47 +0530

    🪓 Code Splitting. Remove unnecessary functions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import { createForm } from "./form.js";
 
-const appid = '2d1d7bee7e67c10f2199eee77d33b57b';
 
-let location = 'Ontario';
+// let location = 'Ontario';
 // let location = prompt('Enter the Location')
 
-async function loadJson(url) {
-    const response = await fetch(url);
-    const weatherJSON = await response.json();
-
-    addInfo(`Temp:  ${weatherJSON.main.temp} C`);
-    addInfo(`Max Temp:  ${weatherJSON.main.temp_max} C`);
-    addInfo(`Min Temp:  ${weatherJSON.main.temp_min} C`);
-    addInfo(`Feels like:  ${weatherJSON.main.feels_like} C`);
-}
-
-function addInfo(text) {
-    const p = document.createElement('p');
-    p.textContent = text;
-    document.body.appendChild(p)
-}
+createForm();
 
 
-loadJson(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${appid}&units=metric`);
 
-// const p = document.createElement('p');
-// p.textContent = weatherData;
-// document.body.append(p);
