3a3158c3ca2556d8605f77769d18a3ddff1e53e8
Arjun <arjunsingh8112@gmail.com>
2023-01-12 14:34:28 +0530

    🧹 Using ternary operator and cleaning code

--- src/form.js
+++ src/form.js
@@ -4,23 +4,11 @@ import { sensitiveData } from "./config.js";
 export function createForm() {
     const form = document.querySelector('form');
     const input = form.querySelector('input');
-    // const submitBtn = form.querySelector('button');
-
-    let locationInput = 'Delhi'
-
+    let locationInput;
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-
-        locationInput = input.value;
-        input.value = '';
-
+        input.value == '' ? locationInput = 'Delhi' : locationInput = input.value;
         console.log(locationInput)
         loadJson(`https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&APPID=${sensitiveData().appid}&units=metric`)
     })
-
-    // return {
-    //     locationInput
-    // }
-
-
 }
