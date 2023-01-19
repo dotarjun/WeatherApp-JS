0819c2de8f1bde0fdbdb3f0f564bed7963828f0a
Arjun <arjunsingh8112@gmail.com>
2023-01-19 14:17:44 +0530

    🧹 Fixing promises and update Image

--- src/form.js
+++ src/form.js
@@ -5,33 +5,20 @@ import { sensitiveData } from "./config.js";
 let locationInput = 'Delhi'
 
 
-async function updateImage() {
-    const img = document.getElementById('weather-image');
-    // const jsondata = await createForm().weatherData;
-    const formData = await createForm()
-    // console.log(`Data ${jsondata}`);
-    console.log(formData);
-    const description = formData.weather[0].description;
-    console.log(description);
-}
-
-
 
 
 export function createForm() {
     const form = document.querySelector('form');
     const input = form.querySelector('input');
-    let jsonData;
-    jsonData = loadJson();
+    let jsonData = loadJson();
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const inputVal = input.value;
         inputVal == '' ? locationInput = 'Delhi' : locationInput = inputVal;
         console.log(locationInput);
         jsonData = loadJson();
-        updateImage()
+        jsonData.resolve(updateImage())
     })
-
     async function loadJson() {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&APPID=${sensitiveData().appid}&units=metric`);
         const weatherJSON = await response.json();
@@ -40,7 +27,20 @@ export function createForm() {
         return weatherJSON;
     }
 
-    return jsonData;
+
+    function updateImage() {
+        const img = document.getElementById('weather-image');
+        // const jsondata = await createForm().weatherData;
+        // const formData = jsonData
+        // console.log(`Data ${jsondata}`);
+        // console.log(formData);
+        const description = jsonData.weather[0].description;
+        console.log(description);
+    }
+
+
+
+    // return jsonData;
     // const jsonData = loadJson().weatherJSON
 }
 
